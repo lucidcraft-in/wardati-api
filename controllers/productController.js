a45d4fe85792f9d77d1d9a7a1ae82187f08a63d5
SALMAN-FRZ <farisareekkad796@gmail.com>
2022-10-19 17:09:48 +0530

    see all product

--- controllers/productController.js
+++ controllers/productController.js
@@ -21,8 +21,8 @@ const getProducts = asyncHandler(async (req, res) => {
 
   const count = await Product.countDocuments({ ...keyword })
   const products = await Product.find({ ...keyword })
-    .limit(pageSize)
-    .skip(pageSize * (page - 1))
+    // .limit(pageSize)
+    // .skip(pageSize * (page - 1))
 
   res.json({ products, page, pages: Math.ceil(count / pageSize) })
 })
