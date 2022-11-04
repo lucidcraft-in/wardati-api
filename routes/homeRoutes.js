863a37d0da6dcaf3ebbfa82ee656ef811d7acc4a
SALMAN-FRZ <farisareekkad796@gmail.com>
2022-11-04 14:54:32 +0530

    home category

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -4,9 +4,8 @@ const router = express.Router();
 
 import { getTopSelling } from '../controllers/stockController.js';
 import { getBanner} from '../controllers/bannerPageController.js';
-
+import { getCategoriesListHomePage } from '../controllers/categoryController.js';
  
-
 
 // Banner
 router.route('/banner/:id').get(getBanner);
@@ -14,5 +13,9 @@ router.route('/banner/:id').get(getBanner);
 
 // Top selling
 router.route('/topselling').get(getTopSelling);
+
+
+// Categories list with sub category and products
+router.route('/categories').get(getCategoriesListHomePage);
 
 export default router;
